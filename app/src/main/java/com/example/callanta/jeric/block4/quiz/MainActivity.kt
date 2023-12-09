b496d651713a9ca393df4fffe53315ebf095fb82
jrccllnt <153366881+jrccllnt@users.noreply.github.com>
2023-12-09 23:42:00 +0800

    Add files via upload

---
+++ app/src/main/java/com/example/callanta/jeric/block4/quiz/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.callanta.jeric.block4.quiz
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.callanta.jeric.block4.quiz.ui.theme.JericCallantablock4quizTheme
+
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JericCallantablock4quizTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JericCallantablock4quizTheme {
+        Greeting("Android")
+    }
+}
