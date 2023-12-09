f8e7e0ece87243e2fae84a967444a02c1e8ecf53
jrccllnt <153366881+jrccllnt@users.noreply.github.com>
2023-12-09 23:35:48 +0800

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "JericCallanta-block4-quiz"
+include(":app")
+ 
